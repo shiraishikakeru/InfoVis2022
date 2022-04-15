a98efad72d8e53397a16792938b8e1a90fdea6d7
Shiraishi Kakeru <shiraishikakeru@ShiraishinoiMac.local>
2022-04-15 17:21:58 +0900

    Initial commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -66,20 +66,13 @@ class Vec3
         this.x = x;
         this.y = y;
         this.z = z;
-        
-        add(v)
-        {
-            this.x += v.x;
-            this.y += v.y;
-            this.z += v.z;
-            return this;
-        }
-        min()
-        {
-            const m = this.x < this.y ? this.x : this.y;
-            return m < this.z ? m : this.z;
-        }
-    
     }
+
+    min()
+    {
+        const m = this.x < this.y ? this.x : this.y;
+        return m < this.z ? m : this.z;
+    }
+
     
 }
