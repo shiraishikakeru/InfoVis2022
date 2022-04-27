5ab1d8e55521aa565a1a540151175db4c9e57890
shiraishikakeru <sheepwing9185@gmail.com>
2022-04-27 14:50:57 +0900

    y軸追加

--- W06/w06_task01.js
+++ W06/w06_task01.js
@@ -55,6 +55,10 @@ class ScatterPlot {
         self.xaxis = d3.axisBottom( self.xscale )
             .ticks(6);
 
+        self.yaxis = d3.axisLeft( self.yscale )
+            .ticks(6);
+
+
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
     }
