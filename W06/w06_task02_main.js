80bae9e1b35377ee25e6249b64da36a888091811
shiraishikakeru <sheepwing9185@gmail.com>
2022-05-08 03:34:26 +0900

    label

--- W06/w06_task02_main.js
+++ W06/w06_task02_main.js
@@ -6,7 +6,7 @@ d3.csv("https://shiraishikakeru.github.io/InfoVis2022/W04/w04_task1.csv")
             parent: '#drawing_region',
             width: 350,
             height: 350,
-            margin: {top:50, right:60, bottom:50, left:60}
+            margin: {top:50, right:60, bottom:50, left:70}
         };
     
         const scatter_plot = new ScatterPlot( config, data );
@@ -55,13 +55,13 @@ class ScatterPlot {
             .ticks(6);
 
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`)
+            .attr('transform', `translate(10, ${self.inner_height})`)
 
         self.yaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, 0)`)
+            .attr('transform', `translate(10, 0)`)
         
         self.xlabel = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height + self.config.margin.top})`)
+            .attr('transform', `translate(${self.inner_width/2}, ${self.inner_height + self.config.margin.top})`)
             .append("text")
             .attr("width", 300)
             .attr("height", 10)
